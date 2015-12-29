7cd10cfd36d4bb28bee2b320c38f02a5c3dae6ed
TheWhiteLlama <das.weisse.lama@gmail.com>
2015-12-29 12:39:22 +0100

    initial commit

---
+++ example/src/main/java/com/llama/android/licenseview/example/MainActivity.java
@@ -0,0 +1,30 @@
+package com.llama.android.licenseview.example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.llama.android.crop.example.R;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
